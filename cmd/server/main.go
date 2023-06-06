ff6a52be36bce86318ea891aa966835e4ff67cab
Matheus Moraes <matheusfaria.moraes@gmail.com>
2023-06-06 14:59:28 -0300

    chore: create server to test

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "listen address")
+	dir    = flag.String("dir", "../../web", "directory to serve")
+)
+
+func main() {
+	flag.Parse()
+	log.Printf("listening on %s...", *listen)
+	err := http.ListenAndServe(*listen, http.FileServer(http.Dir(*dir)))
+	log.Fatalln(err)
+}
