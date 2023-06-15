3c3d8cf35325b8f41c1a7771426ef0b51bdb4ed8
Héricles Emanuel <hericles@getupcloud.com>
2023-06-15 10:58:11 -0300

    chg: feat: remove print margin

--- web/assets/js/editor.js
+++ web/assets/js/editor.js
@@ -20,6 +20,7 @@ class AceEditor {
   constructor(id) {
     this.editor = ace.edit(id);
     this.editor.setTheme(EDITOR_DEFAULTS[id].theme);
+    this.editor.setShowPrintMargin(false);
     this.editor.getSession().setMode(EDITOR_DEFAULTS[id].mode);
     this.editor.getSession().setUseWorker(false);
   }
