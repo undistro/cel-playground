a1480bd0d71e657038dcefe8bd00198deda6a9b2
joaovictor3g <jvdias79797@gmail.com>
2024-04-11 15:53:49 -0300

    removes MODES and rename localStorage key

--- web/assets/js/constants.js
+++ web/assets/js/constants.js
@@ -1,17 +1,1 @@
-const localStorageKey = "@cel-playground:mode";
-
-const MODES = Object.freeze({
-  CEL: { value: "CEL", title: "CEL Expression", html: "" },
-  VAP: {
-    value: "VAP",
-    title: "Validating Admission Policy",
-    html: [{ selector: "", string: "" }],
-  },
-  WEB_HOOKS: { value: "WEB_HOOKS", title: "Web Hooks", html: "" },
-  AUTH_CMAPPING: {
-    value: "AUTH_CMAPPING",
-    title: "Authentication Claim Mapping",
-    html: "",
-  },
-  AUTH: { value: "AUTH", title: "Authentication", html: "" },
-});
+const localStorageKey = "mode";
