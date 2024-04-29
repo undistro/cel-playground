f3fc51a4cbe923913b90a62285c590f6a37d7027
joaovictor3g <jvdias79797@gmail.com>
2024-04-29 11:09:46 -0300

    adds 'variables.'

--- web/assets/js/components/accordions/result.js
+++ web/assets/js/components/accordions/result.js
@@ -66,7 +66,7 @@ function createLabel(item, name, i) {
   arrowIcon.className = "ph ph-caret-right ph-bold result-arrow";
 
   const span = document.createElement("span");
-  span.innerHTML = `${item?.name ? item.name : `${name}[${i}]`}`;
+  span.innerHTML = `${item?.name ? `variables.${item.name}` : `${name}[${i}]`}`;
 
   parentContainer.appendChild(arrowIcon);
 
